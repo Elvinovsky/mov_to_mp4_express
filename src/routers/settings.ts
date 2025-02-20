b16aa372f8a95b58f5155320e7fd439b9489ee72
el <shipagakerimov@mail.ru>
2025-02-20 23:12:17 +0500

    ref

--- src/routers/settings.ts
+++ src/routers/settings.ts
@@ -1,15 +1,13 @@
 import Express from "express";
 import cors from "cors";
 import { errorHandler, loggerMiddleware } from "../middlewares";
-import { mediaRouter } from "./mediaRouter";
 
-export const buildRouter = () => {
+export const buildRouter = (handlers: Express.Router[]) => {
   const router = Express.Router();
 
-  router.use(cors());
-  router.use(Express.json());
+  router.use([cors(), Express.json()]);
   router.use(loggerMiddleware);
-  router.use(mediaRouter);
+  router.use(handlers);
   router.use(errorHandler);
 
   return router;
